d9c5eb438550a8b79cf805213db7b8c4e2c62172
Ilias Vynckier <ilias.vynckier@gmail.com>
2021-12-17 17:06:05 +0100

    first commit

---
+++ build/CMakeFiles/Opdracht6-Kernelmodule.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opdracht6-Kernelmodule.dir/main.c.o"
+  "Opdracht6-Kernelmodule"
+  "Opdracht6-Kernelmodule.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Opdracht6-Kernelmodule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
